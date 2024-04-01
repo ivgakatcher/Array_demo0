d3a5b78c58ffd8ab580172b977ee5efa6c936e2e
ivgakatcher <itavr.working@gmail.com>
2024-04-01 15:36:26 +0300

    add function sum, max, min

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,15 @@ public class Main {
         fillArray(massiv0, 193);
         printArray("массив0 содержит ", massiv0);
         printArray("массив2 содержит ", massiv2);
+        System.out.println("Сумма элементов массива0:"+sumArray(massiv0));
+        System.out.println("Сумма элементов массива1:"+sumArray(massiv1));
+        System.out.println("Сумма элементов массива2:"+sumArray(massiv2));
+        System.out.println("Минимальное значение массива0:"+minArray(massiv0));
+        System.out.println("Минимальное значение массива1:"+minArray(massiv1));
+        System.out.println("Минимальное значение массива2:"+minArray(massiv2));
+        System.out.println("Максимальное значение массива0:"+maxArray(massiv0));
+        System.out.println("Максимальное значение массива1:"+maxArray(massiv1));
+        System.out.println("Максимальное значение массива2:"+maxArray(massiv2));
     }
     private static void printArray(String message, int[] massiv) {
         System.out.println(message + massiv.length + " чисел");
@@ -44,4 +53,27 @@ public class Main {
         readArray(m);
         return m;
     }
+    public static int sumArray(int[] m){
+        int M = 0;
+        for (int i = 0; i < m.length; i++)
+            M = M + m[i];
+        return M;
+    }
+    public static int minArray(int[] m){
+        int min = m[0];
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] < min)
+                min = m[i];
+        }
+        return min;
+    }
+    public static int maxArray(int[] m){
+        int max = m[0];
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] > max)
+                max = m[i];
+        }
+        return max;
+    }
+    
 }
