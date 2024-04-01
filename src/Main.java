c4c1eda02cb3d4c097d1715cc6709f8f0235b5f6
alkaz07 <alkaz07@yandex.ru>
2024-04-01 13:36:17 +0300

    сделал функцию вывода массива в консоль printArray

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,16 @@ public class Main {
         int size = scanner.nextInt();
 
         int[] massiv2 = new int[size];
+
+        printArray("массив0 содержит ", massiv0);
+        printArray("массив1 содержит ", massiv1);
+        printArray("массив2 содержит ", massiv2);
+    }
+
+    private static void printArray(String message, int[] massiv) {
+        System.out.println(message + massiv.length + " чисел");
+        for (int i = 0; i < massiv.length; i++) {
+            System.out.println(massiv[i]);
+        }
     }
 }
