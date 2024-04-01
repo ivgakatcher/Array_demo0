aff7a15782c980ce14a4c0b07409793e6c1dc528
ivgakatcher <itavr.working@gmail.com>
2024-04-01 15:06:01 +0300

    add function inputArray

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,14 @@ public class Main {
         int[] massiv0 = new int[5]; //объявление переменной и выделение памяти для 5 целых чисел
         //массив уже заполнен 0
         int[] massiv1 = {12, 16, 13, -8, 7, 9};
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("введите размер массива 2");
-        int size = scanner.nextInt();
-        int[] massiv2 = new int[size];
+        int[] massiv2 = inputArray();
 
         printArray("массив0 содержит ", massiv0);
         printArray("массив1 содержит ", massiv1);
         printArray("массив2 содержит ", massiv2);
         fillArray(massiv0, 193);
         printArray("массив0 содержит ", massiv0);
-
-        readArray(massiv2);
         printArray("массив2 содержит ", massiv2);
-
     }
     private static void printArray(String message, int[] massiv) {
         System.out.println(message + massiv.length + " чисел");
@@ -27,14 +20,12 @@ public class Main {
             System.out.println(massiv[i]);
         }
     }
-
     public static void fillArray(int[] m, int filler)
     {
         for (int i = 0; i < m.length; i++) {
             m[i] = filler;
         }
     }
-
     public static void readArray(int[] m)
     {
         Scanner scanner = new Scanner(System.in);
@@ -44,5 +35,13 @@ public class Main {
             value = scanner.nextInt();
             m[i] = value;
         }
+    }
+    public static int[] inputArray(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите размер массива 2");
+        int size = scanner.nextInt();
+        int[] m = new int[size];
+        readArray(m);
+        return m;
     }
 }
