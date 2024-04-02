feed63683589ea0dc0f8f5c9b1d5cf1a2e256d55
ivgakatcher <ivgakatcher@itavr.working@gmail.com>
2024-04-02 06:19:45 +0300

    add some functions

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,15 @@ public class Main {
         System.out.println("Минимальное значение массива0:"+minArray(massiv0));
         System.out.println("Минимальное значение массива1:"+minArray(massiv1));
         System.out.println("Минимальное значение массива2:"+minArray(massiv2));
-        System.out.println("Максимальное значение массива0:"+maxArray(massiv0));
-        System.out.println("Максимальное значение массива1:"+maxArray(massiv1));
-        System.out.println("Максимальное значение массива2:"+maxArray(massiv2));
+        System.out.println("Номер максимального элемента массива0:"+indexOfMaxArray(massiv0));
+        System.out.println("Номер максимального элемента массива1:"+indexOfMaxArray(massiv1));
+        System.out.println("Номер максимального элемента массива2:"+indexOfMaxArray(massiv2));
+        MaxMinArray(massiv2);
+        System.out.println("Номер максимального элемента массива2:"+indexOfMaxArray(massiv2));
+        System.out.println("Элемент равный параметру Х массива2:"+findXArray(massiv2));
+        System.out.println("Номер элемента ближайшего по модулю к Х массива2:"+findModCloseXArray(massiv2));
+        printBackArray("массив2 содержит ", massiv2);
+        printIntervalArray(massiv1);
     }
     private static void printArray(String message, int[] massiv) {
         System.out.println(message + massiv.length + " чисел");
@@ -54,26 +60,84 @@ public class Main {
         return m;
     }
     public static int sumArray(int[] m){
-        int M = 0;
+        int msum = 0;
         for (int i = 0; i < m.length; i++)
-            M = M + m[i];
-        return M;
+            msum = msum + m[i];
+        return msum;
     }
     public static int minArray(int[] m){
         int min = m[0];
-        for (int i = 0; i < m.length; i++) {
+        for (int i = 1; i < m.length; i++) {
             if (m[i] < min)
                 min = m[i];
         }
         return min;
     }
-    public static int maxArray(int[] m){
-        int max = m[0];
-        for (int i = 0; i < m.length; i++) {
-            if (m[i] > max)
-                max = m[i];
+    public static int indexOfMaxArray(int[] m){
+        int imax = 0;
+        for (int i = 1; i < m.length; i++) {
+            if (m[i] > m[imax]) {
+                imax = i;
+            }
         }
-        return max;
+        return imax;
     }
-    
+    public static int indexOfMinArray(int[] m){
+        int imin = 0;
+        for (int i = 1; i < m.length; i++) {
+            if (m[i] < m[imin]) {
+              imin = i;
+            }
+        }
+        return imin;
+    }
+    public static void MaxMinArray(int[] m){
+        int imin = indexOfMinArray(m);
+        int imax = indexOfMaxArray(m);
+        int temp;
+        temp = m[imin];
+        m[imin] = m[imax];
+        m[imax] = temp;
+    }
+    public static int findXArray(int[] m){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число х: ");
+        int x = sc.nextInt();
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] == x)
+                return i;
+        }
+        return -1;
+    }
+    public static int findModCloseXArray(int[] m){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число х: ");
+        int x = sc.nextInt();
+        int modClose = m[0];
+        int iclose = 0;
+        for (int i = 1; i < m.length; i++) {
+            if (Math.abs(Math.abs(x) - Math.abs(m[i])) < Math.abs((Math.abs(x) - modClose))) {
+                modClose = m[i];
+                iclose = i;
+            }
+        }
+        return iclose;
+    }
+    private static void printBackArray(String message, int[] massiv) {
+        System.out.println(message + massiv.length + " чисел");
+        for (int i = massiv.length-1; i == 0; i--) {
+            System.out.println(massiv[i]);
+        }
+    }
+    public static void printIntervalArray(int[] m){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число х1: ");
+        int x1 = sc.nextInt();
+        System.out.println("Введите число х2: ");
+        int x2 = sc.nextInt();
+        for (int i = 0; i < m.length; i++) {
+            if ((m[i] >= x1) && (m[i] <= x2))
+                System.out.println(m[i]);
+        }
+    }
 }
